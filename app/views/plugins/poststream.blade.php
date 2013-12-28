6cf73cd80ddb09779cd920c792911060cd1fd685
ricomonster <riconess@gmail.com>
2013-12-29 02:26:24 +0800

    fixed showing of assignment buttons

--- app/views/plugins/poststream.blade.php
+++ app/views/plugins/poststream.blade.php
@@ -96,10 +96,10 @@
                     ?>
                             <strong>{{{ $post->assignment->title }}}</strong>
                             <div class="assignment-details">
-                                @if(Auth::user()->id == 1)
+                                @if(Auth::user()->account_type == 1)
                                 <a href="#" class="btn btn-default">Turned In (0)</a>
                                 @endif
-                                @if(Auth::user()->id == 2)
+                                @if(Auth::user()->account_type == 2)
                                 <a href="/assignment-sheet/{{ $post->assignment->assignment_id }}"
                                 class="btn btn-default">Turn In</a>
                                 @endif
