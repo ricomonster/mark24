812c3ba5b9a252d4d73b48ceee93a7ef291df75c
ricomonster <rmaglayon@gmail.com>
2014-03-03 15:23:25 +0800

    fixing

--- app/models/Auxillary.php
+++ app/models/Auxillary.php
@@ -3,8 +3,8 @@ class Auxillary
 {
     public static function groupChats()
     {
-        $chats = Conversation::groupChats();
-        return View::make('ajax.chat.chatbox')
-            ->with('chats', $chats);
+        // $chats = Conversation::groupChats();
+        // return View::make('ajax.chat.chatbox')
+        //     ->with('chats', $chats);
     }
 }
